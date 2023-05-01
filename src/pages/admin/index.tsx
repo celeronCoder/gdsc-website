3283fbc285bb6eda07ba5b7cedf64eeb117bd47c
Khushal Bhardwaj <76873719+celeronCoder@users.noreply.github.com>
2023-05-01 03:55:11 +0000

    the admin homepage is complete

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -5,6 +5,7 @@ import { Lato } from "next/font/google";
 import styles from "./admin.module.css";
 import Head from "next/head";
 import { signOut } from "next-auth/react";
+import Link from "next/link";
 
 const lato = Lato({ weight: ["400", "700"], subsets: ["latin"] });
 
@@ -15,8 +16,32 @@ export default function AdminPage() {
         <title>Admin | GDSC VIT Bhopal</title>
       </Head>
       <div style={lato.style} className={styles.main}>
-        admin page
-        <button onClick={() => signOut()}>log out</button>
+        <nav className={styles.nav}>
+          {/* the primary Link */}
+          <div>
+            <Link href="/admin" className={styles.primaryLink}>
+              GDSC VITB Admin
+            </Link>
+          </div>
+
+          {/* the other Links */}
+          <div className={styles.navLinks}>
+            <Link href={"/admin/events"} className={styles.link}>
+              Events
+            </Link>
+          </div>
+
+          {/* the logout */}
+          <div>
+            <button onClick={() => signOut()} className={styles.logoutBtn}>
+              Log Out
+            </button>
+          </div>
+        </nav>
+        <main className={styles.container}>
+          <h1>Welcome to the GDSC VIT Bhopal's Admin Page</h1>
+          <p className={styles.secondary}>This is the page only for Admins</p>
+        </main>
       </div>
     </>
   );
